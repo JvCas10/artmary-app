e0dfeb975aaa4b42d72ef62f4caf84d780b5f810
JvCas10 <jcastellanosp3@miumg.edu.gt>
2025-08-05 17:58:10 -0600

    Fix correo 13

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { HashRouter } from 'react-router-dom'; // CAMBIAR ESTA LÍNEA
+import { BrowserRouter } from 'react-router-dom';
 import { CartProvider } from './context/CartContext';
+import { AuthProvider } from './context/AuthContext.jsx'; // <-- ¡AGREGA ESTA LÍNEA!
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter> {/* CAMBIAR ESTA LÍNEA */}
+    <BrowserRouter>
       <CartProvider>
-        <App />
+        <AuthProvider> {/* <-- ¡ENVUELVE App CON AuthProvider AQUÍ! */}
+          <App />
+        </AuthProvider>
       </CartProvider>
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
